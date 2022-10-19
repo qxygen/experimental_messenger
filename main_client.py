513b62af27663ff5bd2b6d34e2eecc92b15e3013
qxygen <52496357+qxygen@users.noreply.github.com>
2022-10-19 19:47:47 +0300

    Update main_client.py

--- main_client.py
+++ main_client.py
@@ -3,32 +3,41 @@ import time
 import threading
 from itertools import groupby
 
+zero_time = time.time()
+
+
+def clean_data(s: str, symbol='&'):
+    pos = s.find(symbol)
+    while pos != -1:
+        s = s[:pos] + s[pos + 1:]
+        pos = s.find(symbol)
+    return s
+
 
 def response_func(data) -> str:
-    print(data)
+    print(f"client: {data}")
     resp = input("input: ")
-    return resp
+    return f"-=-main-=-time: {round(time.time() - zero_time)}s, ans: {resp}"
 
 
 def online_check(client_socket: socket.socket):
     try:
         client_socket.send("&".encode('utf-8'))
-        # print(f"online check client {client_socket}: true")
+
         return True
     except ConnectionResetError:
-        # print(f"online check client {client_socket}: false")
+
         return False
 
 
 class Client:
     def __init__(self):
-        self.zero_time = time.time()
+        self.zero_time = zero_time
         self.server_ip = '192.168.43.150'
         self.server_port = 5556
 
         self.server_socket = 0
         self.connect()
-        # self.server_socket.send('-=-main-=-first message from main client'.encode('utf-8'))
 
     def connect(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,23 +58,19 @@ class Client:
     def main_loop(self, server_socket_: socket.socket):
         while True:
             try:
-                # print('.')
                 data = '&'
-                # data = server_socket_.recv(1024).decode('utf-8')
+
                 while [el for el, _ in groupby(sorted(data.split('&')))] == ['']:
                     data = self.server_socket.recv(1024).decode('utf-8')
-                    # print("while ", data)
-                # data = data
-                # print(data.split("-=-")[1::])
+                data = clean_data(data)
                 data_array = data.split("-=-")[1::]
                 if data_array[0] == 'nm':
                     ans = response_func(data_array[1])
-                    server_socket_.send(f"-=-main-=-time: {round(time.time() - self.zero_time)}s, ans: {ans}".encode('utf-8'))
+                    server_socket_.send(ans.encode('utf-8'))
+
                 else:
                     print(f"{data_array[0]}: {data_array[1]}")
                     pass
-                # except ValueError:
-                    # print(f"error occured: {data.split('-=-')}")
 
             except ConnectionResetError:
                 self.connect()
